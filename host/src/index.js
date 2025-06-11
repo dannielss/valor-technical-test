457928f0b97b2c548ce4c0fdbca8e6b660c65c70
Daniel Sousa <daniel_sousa.p@outlook.com>
2025-06-11 02:05:40 -0300

    feat: set remote url and display remote app and host app divs

--- host/src/index.js
+++ host/src/index.js
@@ -1,5 +1,6 @@
 // You can write your own logic here to determine the actual url
-window.remoteUrl = "";
+window.remoteUrl =
+  "https://daniel-de-sousa-24-remote-valor-technical-test-da-87b584d64-ze.zephyrcloud.app";
 
 // Use dynamic import here to allow webpack to interface with module federation code
 import("./bootstrap");
